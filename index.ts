5bd0da295dc3cfd35461eccb35cf657844d8b3f7
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-08-12 20:45:56 +0500

    added some colors in game`s life

--- index.ts
+++ index.ts
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
-
 import inquirer from "inquirer";
-const answers = await inquirer.prompt([]);
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-// console.log(randomNumber);
+import chalk from "chalk";
+import showBanner from "node-banner" ;
+import chalkAnimation from 'chalk-animation';
 
-const answer = await inquirer.prompt([
-  {
-    name: "userGuesedNumber",
-    type: "number",
-    message: "\t Please Guess a number between 1 to 6 :\t \n ",
-  },
-]);
-console.log(answer);
-if (answer.userGuesedNumber === randomNumber) {
-  console.log("congratulation! you guessed the right number.");
-} else {
-  console.log("you guessed the wrong number");
-}
+ 
+(async () => {
+    await showBanner('G u e s s   a N u m ber', '\t \t \t Between 1 to 6 \n','green' , 'blue');
+
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    console.log( 'answer to cheat 🤫: ', randomNumber);
+    
+    const answer = await inquirer.prompt([
+      {
+        name: "userGuessedNumber",
+        type: "number",
+        message: chalkAnimation.rainbow("\t Please Guess a number between 1 to 6 :\t \n "),
+      },
+    ]);
+    console.log(chalk.bgWhiteBright(chalk.black.bold('You Enter')), answer.userGuessedNumber);
+    if (answer.userGuessedNumber === randomNumber) {
+      console.log(chalk.rgb(242, 11, 151).bold.italic("congratulation!🎉✨") ,chalk.rgb(242, 158, 200)('you guessed the right number.🎊'))
+    } else {
+      console.log(chalk.rgb(242, 0, 0).bold.italic("you guessed the wrong number")+'😢😢😢');
+    }
+
+})();
+
+
